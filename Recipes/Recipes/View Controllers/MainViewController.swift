c9f8a6481e9c9f4b65c0b9d703c03906111d6724
Marc Jacques <Marcjacques@me.com>
2019-11-09 09:42:47 -0500

    Modified storyboard to make simpler tableView

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -9,26 +9,55 @@
 import UIKit
 
 class MainViewController: UIViewController {
+   
     
-    @IBOutlet weak var searchTextField: UITextField!
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = []
+    var recipesTableViewController: RecipesTableViewController?
+    var filteredRecipes: [Recipe] = []
+    var searchController: UISearchController!
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (allRecipes, error) in
+            if let error = error {
+                NSLog("Error while fetching all recipes: \(error)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let recipes = allRecipes {
+                    self.filteredRecipes = recipes
+                }
+            }
+            
+            // Do any additional setup after loading the view.
+        }
+       
+    
     }
     
-    @IBAction func searchTextField(_ sender: Any) {
+  
+       
+    
+    func filteredReciptes() {
+        
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "RecipeTableView" {
+            // Get the new view controller using segue.destination.
+            recipesTableViewController = segue.destination as? RecipesTableViewController
+            // Pass the selected object to the new view controller.
+        }
+    
+     }
+     
+    
 }
+
--- Recipes/Recipes/View Controllers/RecipesTableViewController.swift
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -96,5 +96,4 @@ class RecipesTableViewController: UITableViewController {
             }
         }
     }
-        
 }
